39d4c4009404475a015d3ec6642ef265dc622ccc
saqi018 <saqibkn0018@gmail.com>
2024-02-23 16:14:50 +0500

    functionTask

---
+++ index.js
@@ -0,0 +1,70 @@
+
+// ************************** Task No 3 /////////////////////////
+
+/*
+function welcomeMassage() {
+  let userName = prompt("Enter your Name");
+   alert("welcome" + " " + userName);
+ }
+
+ welcomeMassage(); 
+ */
+
+
+
+// ****************************** Task No 2 *******************************
+
+
+/*
+function generateQuote() {
+     let quote = [ "The only way to do great work is to love what you do.",
+     "In three words I can sum up everything I've learned about life: it goes on.",
+     "Believe you can and you're halfway there.",
+     "Your time is limited, don't waste it living someone else's life. - ",
+     "The future belongs to those who believe in the beauty of their dreams."
+]
+    let randomIndex = Math.floor(Math.random() * quote.length);
+    let randomQuote = quote[randomIndex];
+
+    console.log(randomQuote);
+}
+
+generateQuote(); */
+
+
+
+
+
+ // ************************** Task No 3 ************************
+
+
+/*
+function generateNUmber() {
+    let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+    console.log(randomNumber);
+    
+}
+
+generateNUmber(); 
+
+*/
+
+
+
+  var abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+function displayAlphabets(){
+  var randomAlphabets = Math.floor(Math.random() * abc.length);
+   return abc[randomAlphabets];
+  
+
+}
+
+function display(){
+  var alphabets = displayAlphabets();
+  console.log(abc);
+}
+
+
+display();
